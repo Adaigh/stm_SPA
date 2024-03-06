6faa1dffe4be13d73cce3b59f90f2e824ecddccc
Adaigh <adaigh@asu.edu>
2024-03-06 15:26:32 -0800

    Adding images to landing page

--- frontend/src/pages/STMHome.js
+++ frontend/src/pages/STMHome.js
@@ -1,18 +1,20 @@
 import AboutPane from '../components/AboutPane.js'
 import NewCarousel from '../components/Carousel_two.js';
 
+
+
 const sectionData = [
     {
         sectionTitle: 'Performance:',
         sectionContent: 'Our team is passionate about performance... (rest of content)',
-        src: './images/engine1.jpg',
+        src: process.env.PUBLIC_URL + '/images/engine1.jpg',
         alt: 'Built engine',
         caption: 'Freshly built 2.0'
     },
     {
         sectionTitle: 'Repairs:',
         sectionContent: 'Putting your vehicle through its paces... (rest of content)',
-        src: './images/dave1.jpg',
+        src: process.env.PUBLIC_URL + '/images/dave1.jpg',
         alt: 'Portrait - Dave K.',
         caption: 'Dave K. - Owner, Lead Mechanic'
     }
