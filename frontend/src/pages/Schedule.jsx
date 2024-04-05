71b012b83064c916d8c703d40ce938882d0eca8d
Adaigh <adaigh@asu.edu>
2024-04-05 08:09:50 -0700

    Began implementation of staff schedule page

--- frontend/src/pages/Schedule.jsx
+++ frontend/src/pages/Schedule.jsx
@@ -1,6 +1,125 @@
+import { useState } from 'react';
+
 const Schedule = () => {
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [monthly, setMonthly] = useState(false)
+
     return (
-        <h1>"Test"</h1>
+        <div className='schedule'>
+            <h1>{currentDate.toLocaleDateString()}</h1>
+            <button onClick={
+                () => monthly ? setMonthly(false) : setMonthly(true)
+            }>{monthly && <>Week</>}{!monthly && <>Month</>} View</button>
+            <div className='scheduleDisplay'>
+            {!monthly &&
+                <>
+                    <div>Monday</div>
+                    <div>Tuesday</div>
+                    <div>Wednesday</div>
+                    <div>Thursday</div>
+                    <div>Friday</div>
+                </>
+            }
+            {monthly &&
+                <>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                    <div>
+                        <p>TEST</p>
+                    </div>
+                </>
+            }
+            </div>
+        </div>
     )
 }
 
