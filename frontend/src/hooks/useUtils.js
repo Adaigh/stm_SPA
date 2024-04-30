89565956625ff59d603f99f24eed930dfcb47e7f
Adaigh <adaigh@asu.edu>
2024-04-30 15:13:32 -0700

    Adding capitalization utility function

--- frontend/src/hooks/useUtils.js
+++ frontend/src/hooks/useUtils.js
@@ -1,3 +1,7 @@
 export const formatPhone = (num) => {
     return num.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')
 }
+
+export const capitalize = (str) => {
+    return (str.charAt(0).toUpperCase() + str.slice(1))
+}
