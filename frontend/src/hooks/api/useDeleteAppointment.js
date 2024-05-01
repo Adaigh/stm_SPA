f361ab008cf840bc3fb8520d54db31e08ce71261
Adaigh <adaigh@asu.edu>
2024-05-01 14:27:11 -0700

    Fixing hook implementation

--- frontend/src/hooks/api/useDeleteAppointment.js
+++ frontend/src/hooks/api/useDeleteAppointment.js
@@ -1,16 +1,38 @@
 import { useAuthContext } from "../useAuthContext"
+import { useScheduleContext } from "../useScheduleContext";
 
-export const deleteAppointment = async (id) => {
+export const useDeleteAppointment = () => {
 
-    const { user } = useAuthContext();
+    const {user} = useAuthContext();
+    const {schedule, dispatch} = useScheduleContext()
 
-    const request = {
-        method: 'DELETE',
-        headers: {
-            'Authorization': `Bearer ${user.webToken}`
+    const deleteApp = async (appReq) => {
+
+        const request = {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.webToken}`
+            }
         }
+
+        const response = await fetch('/api/appointments/' + appReq._id, request)
+        const json = response.json()
+
+        let message = ''
+        if(response.ok){
+            message = "Appointment removed successfully"
+
+            let updatedAppointments = schedule.filter((app)=> {
+                return app !== appReq
+            })
+
+            dispatch({type: 'SET_SCHEDULE', payload: updatedAppointments})
+
+        } else {
+            message = json.error
+        }
+        return message
     }
 
-    const response = await fetch('/api/appointments/' + id, request)
-    return response
+    return {deleteApp}
 }
