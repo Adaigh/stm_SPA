f0768fcd6bae206d1e2a37388e8c744d96959632
Adaigh <adaigh@asu.edu>
2024-06-14 10:03:41 -0700

    Formatting corrections

--- frontend/src/hooks/api/useCustomersApi.js
+++ frontend/src/hooks/api/useCustomersApi.js
@@ -5,8 +5,8 @@ import { api_url } from "../../production_variables"
 // Create a new customer record
 export const useCreateCustomer = () => {
 
-    const {user} = useAuthContext()
-    const {dispatch} = useCustomersContext()
+    const { user } = useAuthContext()
+    const { dispatch } = useCustomersContext()
 
     const createCustomer = async (newCustomer) => {
         const response = await fetch(`${api_url}/api/customers`, {
@@ -19,70 +19,73 @@ export const useCreateCustomer = () => {
         })
 
         const json = await response.json()
-        if(response.ok) {
-            dispatch({type: 'CREATE_CUSTOMER', payload: json})
+        if (response.ok) {
+            dispatch({ type: 'CREATE_CUSTOMER', payload: json })
         }
-        return {response, json}
+        return { response, json }
     }
-    return {createCustomer}
+    return { createCustomer }
 }
 
 // Get all customer details
 export const useFetchCustomers = () => {
 
-    const {user} = useAuthContext()
-    const {dispatch} = useCustomersContext()
+    const { user } = useAuthContext()
+    const { dispatch } = useCustomersContext()
 
     const fetchCustomers = async () => {
 
         const response = await fetch(`${api_url}/api/customers/`, {
             headers: {
                 'Authorization': `Bearer ${user.webToken}`
-            }    
-        })    
-        
+            }
+        })
+
         const json = await response.json()
-        if(response.ok) {
-            dispatch({type:'SET_CUSTOMERS', payload: json})
-        }    
-    }    
-    return {fetchCustomers}
-    
-}    
+        if (response.ok) {
+            dispatch({ type: 'SET_CUSTOMERS', payload: json })
+        }
+    }
+    return { fetchCustomers }
+
+}
 
 // Update a customer record
 export const useUpdateCustomer = () => {
 
-    const {user} = useAuthContext()
-    const {customers, dispatch} = useCustomersContext()
+    const { user } = useAuthContext()
+    const { customers, dispatch } = useCustomersContext()
 
     const updateCustomer = async (updatedInfo, customer) => {
         const response = await fetch(`${api_url}/api/customers/` + customer._id, {
             method: 'PATCH',
-            headers: {'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${user.webToken}`},
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.webToken}`
+            },
             body: JSON.stringify(updatedInfo)
         })
 
         const json = await response.json()
-        if(response.ok) {
-            window.alert("Updates Successful!")
+        if (response.ok) {
             let newCustomersInfo = [...customers]
             newCustomersInfo[newCustomersInfo.indexOf(customer)] = updatedInfo
-            dispatch({type: 'SET_CUSTOMERS', payload: newCustomersInfo})
+            dispatch({ type: 'SET_CUSTOMERS', payload: newCustomersInfo })
+            window.alert("Updates Successful!")
         }
+        console.log(response, json)
 
-        return {response, json}
+        return { response, json }
     }
 
-    return{updateCustomer}
+    return { updateCustomer }
 }
 
 // Delete a customer record
 export const useDeleteCustomer = () => {
 
-    const {user} = useAuthContext()
-    const {dispatch} = useCustomersContext()
+    const { user } = useAuthContext()
+    const { dispatch } = useCustomersContext()
 
     const deleteCustomer = async (customerInfo) => {
         const response = await fetch(`${api_url}/api/customers/` + customerInfo._id, {
@@ -93,10 +96,10 @@ export const useDeleteCustomer = () => {
         })
 
         const json = await response.json()
-        if(response.ok) {
-            dispatch({type: 'DELETE_CUSTOMER', payload: json})
+        if (response.ok) {
+            dispatch({ type: 'DELETE_CUSTOMER', payload: json })
         }
     }
 
-    return {deleteCustomer}
+    return { deleteCustomer }
 }
