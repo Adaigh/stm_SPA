3dc11168819b62d9c8a3a6a533563669de077f1b
Adaigh <adaigh@asu.edu>
2024-06-14 10:09:55 -0700

    Adding Administration route, plus formatting corrections.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,50 +10,55 @@ import SelfSignup from './pages/SelfSignup'
 import Calendar from './pages/Calendar'
 import Schedule from './pages/Schedule'
 import AccountDetail from './pages/AccountDetail'
+import Administration from './pages/Administration'
 
 
 function App() {
 
-  const {user} = useAuthContext()
+  const { user } = useAuthContext()
 
   return (
     <div className="App">
-      
+
       <BrowserRouter>
 
-      <NavBar/>
+        <NavBar />
         <div className="pages">
-        <Routes>
-              <Route
-                path="/"
-                element={<STMHome />}
-                />
-              <Route
-                path="/calendar"
-                element={<Calendar/>}
-                />
-              <Route
-                path="/schedule"
-                element={user ? <Schedule /> : <Navigate to="/login"/>}
-                />
-              <Route
-                path="/customers"
-                element={user ? <CustomerHome /> : <Navigate to="/login"/>}
-                />
-              <Route
-                path="/details"
-                element={user ? <AccountDetail /> : <Navigate to="/login"/>}
-                />
-              <Route
-                path="/login"
-                element={!user ? <Login /> : <Navigate to="/"/>}
-                />
-              <Route
-                path="/selfsignup"
-                element={!user ? <SelfSignup /> : <Navigate to="/"/>}
-                />
-            </Routes>
-        </div>      
+          <Routes>
+            <Route
+              path="/"
+              element={<STMHome />}
+            />
+            <Route
+              path="/calendar"
+              element={<Calendar />}
+            />
+            <Route
+              path="/schedule"
+              element={user && user.access > 0 ? <Schedule /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/customers"
+              element={user && user.access > 0 ? <CustomerHome /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/admin"
+              element={user && user.access > 0 ? <Administration /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/details"
+              element={user ? <AccountDetail /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/selfsignup"
+              element={!user ? <SelfSignup /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
