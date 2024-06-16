0a9efbb87910760aaf014ab3627bd2994cafe325
Adaigh <adaigh@asu.edu>
2024-06-16 15:38:26 -0700

    Refined deletion acceess based on record type

--- frontend/src/components/ui/CustomerDetails.jsx
+++ frontend/src/components/ui/CustomerDetails.jsx
@@ -7,29 +7,29 @@ import { useDeleteCustomer } from '../../hooks/api/useCustomersApi'
 
 import './styles/CustomerDetails.css'
 
-const CustomerDetails = ({customerInfo}) => {
+const CustomerDetails = ({ customerInfo }) => {
 
     const [expanded, setExpanded] = useState(false)
     const [edit, setEdit] = useState(false)
 
-    const {deleteCustomer} = useDeleteCustomer()
+    const { deleteCustomer } = useDeleteCustomer()
 
     const toggleExpand = () => {
-        if(!expanded){
+        if (!expanded) {
             setExpanded(true)
             return
         } else {
-            if(edit) {
-                if(!window.confirm("Updates NOT saved, continue?")) return
+            if (edit) {
+                if (!window.confirm("Updates NOT saved, continue?")) return
                 setEdit(false)
             }
             setExpanded(false)
         }
     }
-    
+
     const handleDelete = async () => {
 
-        if(!window.confirm("Are you sure you want to delete user " + customerInfo.firstName + " " + customerInfo.lastName + "?")){
+        if (!window.confirm("Are you sure you want to delete user " + customerInfo.firstName + " " + customerInfo.lastName + "?")) {
             return;
         }
 
@@ -47,46 +47,51 @@ const CustomerDetails = ({customerInfo}) => {
                         {!expanded && <span> &emsp; {formatPhone(customerInfo.phoneNumbers[0])}</span>}
                     </h3>
                 </div>
-                <span className='material-symbols-outlined delete' onClick={handleDelete}>delete</span>
+                {customerInfo.emailAddress.includes('(SHOP)') &&
+                    <span>(SHOP RECORD) <span className='material-symbols-outlined delete' onClick={handleDelete}>delete</span></span>
+                }
+                {!customerInfo.emailAddress.includes('(SHOP)') &&
+                    <span>(USER ACCOUNT)</span>
+                }
             </div>
-            {expanded && 
-            
+            {expanded &&
+
                 <>
-                {!edit && 
-                    <>
-                    <div className='info-columns'>
-                        {/* Listing phone numbers */}
-                        <ul>
-                            <li><u>Phone Numbers</u>:&emsp;</li>
-                            {customerInfo.phoneNumbers.map((number, index) => {
-                                return <li key={index}>{number.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')}</li>
-                            })}
-                        </ul>
-                        <ul>
-                            <li><u>Email Address</u>:&emsp;</li>
-                            <li>{customerInfo.emailAddress}</li>
-                        </ul>
-                        <ul>
-                            {/* List vehicles */}
-                            <li><u>Vehicles</u>:&emsp;</li>
-                            {customerInfo.vehicles.map((vehicle, index) => {
-                                return  <li key={index}>
+                    {!edit &&
+                        <>
+                            <div className='info-columns'>
+                                {/* Listing phone numbers */}
+                                <ul>
+                                    <li><u>Phone Numbers</u>:&emsp;</li>
+                                    {customerInfo.phoneNumbers.map((number, index) => {
+                                        return <li key={index}>{number.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3')}</li>
+                                    })}
+                                </ul>
+                                <ul>
+                                    <li><u>Email Address</u>:&emsp;</li>
+                                    <li>{customerInfo.emailAddress}</li>
+                                </ul>
+                                <ul>
+                                    {/* List vehicles */}
+                                    <li><u>Vehicles</u>:&emsp;</li>
+                                    {customerInfo.vehicles.map((vehicle, index) => {
+                                        return <li key={index}>
                                             {vehicle.vehicleYear}&nbsp;
                                             {vehicle.vehicleMake}&nbsp;
                                             {vehicle.vehicleModel} &nbsp;
                                             VIN: {vehicle.vehicleVIN}</li>
-                            })}
-                        </ul>
-                    </div>
-                    <button className='edit'
-                        onClick={(e) => {(e).preventDefault(); setEdit(true)}}>Edit</button>
-                    </>
-                }
-                {edit && 
-                    <CustomerUpdateForm customer={customerInfo}
-                    closeForm={() => setEdit(false)}/>
-                }
-            </>
+                                    })}
+                                </ul>
+                            </div>
+                            <button className='edit'
+                                onClick={(e) => { (e).preventDefault(); setEdit(true) }}>Edit</button>
+                        </>
+                    }
+                    {edit &&
+                        <CustomerUpdateForm customer={customerInfo}
+                            closeForm={() => setEdit(false)} />
+                    }
+                </>
             }
         </div>
     )
