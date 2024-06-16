6bc6c422defa46b671e74cbec1fc03fe7d5faece
Adaigh <adaigh@asu.edu>
2024-06-16 16:10:46 -0700

    Updating contact information links and style

--- frontend/src/components/ui/ContactInfo.jsx
+++ frontend/src/components/ui/ContactInfo.jsx
@@ -2,6 +2,7 @@ import './styles/ContactInfo.css'
 
 const ContactInfo = () => {
     const phoneNumber = '(509) 893-2367'
+    const email = 'stmtuning@hotmail.com'
     const address = '4715 E Trent Ave, Spokane, WA 99212'
     const hours = '9am-5pm, Monday through Friday'
 
@@ -9,8 +10,22 @@ const ContactInfo = () => {
         <div className='contact'>
             <h4>Contact:</h4>
             <ul>
-                <li><span>phone</span> | {phoneNumber}</li>
-                <li><span>address</span> | {address}</li>
+                <li>
+                    <a href="tel:509-893-2367">
+                        <span>phone</span> | {phoneNumber}
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:stmtuning@hotmail.com">
+                        <span>email</span> | {email}
+                    </a>
+                </li>
+                <li>
+                    <a href="https://maps.app.goo.gl/QbksovXGtDDT5ptB9"
+                        target='_blank'>
+                        <span>address</span> | {address}
+                    </a>
+                </li>
                 <li><span>hours</span> | {hours}</li>
             </ul>
         </div>
